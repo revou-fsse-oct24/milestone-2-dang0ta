36ddc1e37418fd10d32f0da484cc4b68043d8e26
dang0ta <ikosiriki@gmail.com>
2025-01-09 17:04:15 +0700

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/404";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="products" element={<Products />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
