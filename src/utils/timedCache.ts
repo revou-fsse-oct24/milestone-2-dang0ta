b3bcd5202662f9ad1d6ff7952349a9e24ead5299
dang0ta <ikosiriki@gmail.com>
2025-01-10 13:03:08 +0700

    simplify how timed-cache works

--- src/utils/timedCache.ts
+++ src/utils/timedCache.ts
@@ -1,56 +1,48 @@
-
 const FIVE_MINUTES = 5 * 60 * 1000;
 
+const getCacheKey = (roundingValue: number = FIVE_MINUTES) =>
+  `cache_${Math.floor(new Date().getTime() / roundingValue) * roundingValue}`;
+
+
 /**
- * get cache key that will returns the exact same value for every {roundingValue} time-block,
- * effectively creating a cache key that expires every {roundingValue} milliseconds.
- * @param roundingValue 
- * @returns 
+ * A utility class that provides a simple timed cache mechanism using localStorage.
+ * The cache stores values with a time-based key and evicts expired entries.
+ * 
+ * This cache uses no timer, the time-based eviction is achieved by constructing 
+ * cache key in a way that the key is always going to be the exact same if generated 
+ * within the same time block, with the default time block being 5 minutes.
+ *
+ * @template T - The type of the value to be stored in the cache.
  */
-export function getCacheKey(roundingValue: number = FIVE_MINUTES) {
-    const now = new Date().getTime();
-
-    // Calculate the start of the 5-minute block in milliseconds
-    const blockStart = Math.floor(now / roundingValue) * roundingValue; 
-  
-    return `cache_${blockStart}`;
-}
-
 export class TimedCache<T> {
-    private readonly timeout: number;
-    private readonly prefix: string;
+  private readonly timeout: number;
+  private readonly prefix: string;
 
-    constructor(prefix: string = "cache",timeout: number = FIVE_MINUTES) {
-        this.timeout = timeout;
-        this.prefix = prefix;
+  constructor(prefix: string = "cache", timeout: number = FIVE_MINUTES) {
+    this.timeout = timeout;
+    this.prefix = prefix;
+  }
+
+  set(value: T) {
+    const current: Record<string, T> = JSON.parse(
+      localStorage.getItem(this.prefix) || "{}"
+    );
+    current[getCacheKey(this.timeout)] = value;
+    localStorage.setItem(this.prefix, JSON.stringify(current));
+  }
+
+  get(): T | undefined {
+    const currentKey = getCacheKey(this.timeout);
+    const current: Record<string, T> = JSON.parse(
+      localStorage.getItem(this.prefix) || "{}"
+    );
+
+    const value = current[currentKey];
+    if (!value) {
+        localStorage.removeItem(this.prefix);
+        return undefined;
     }
 
-    set(value: T) {
-        const current: Record<string, T> = JSON.parse(localStorage.getItem(this.prefix) || "{}");
-        current[getCacheKey(this.timeout)] =value;
-
-        localStorage.setItem(this.prefix, JSON.stringify(current));
-    }
-
-    get(): T | undefined {
-        const currentKey = getCacheKey(this.timeout);
-        let current: Record<string, T> = JSON.parse(localStorage.getItem(this.prefix) || "{}");
-
-        const value = current[currentKey];
-        if (value) {
-            current = this.evictExpired(currentKey, current);
-            localStorage.setItem(this.prefix, JSON.stringify(current));
-        }
-        return value;
-    }
-
-    private evictExpired(current: string, items: Record<string, T>): Record<string, T> {
-        for (const key in items) {
-            if (key !== current) {
-                delete(items[key]);
-            }
-        }
-
-        return items;
-    }
+    return value;
+  }
 }
