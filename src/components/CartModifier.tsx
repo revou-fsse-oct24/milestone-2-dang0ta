b73df12e849b234be2200be0d09aad0921df2802
dang0ta <ikosiriki@gmail.com>
2025-01-10 21:49:53 +0700

    add shadcn

--- src/components/CartModifier.tsx
+++ src/components/CartModifier.tsx
@@ -2,22 +2,28 @@ import { useCart } from "@contexts/CartContext";
 import { Product } from "@models/product";
 import { MinusCircleIcon, PlusCircleIcon } from "lucide-react";
 
-export const CartModifier = ({product}: {product: Product}) => {
-    const {get, addItem, removeItem} = useCart();
+export const CartModifier = ({ product }: { product: Product }) => {
+  const { get, addItem, removeItem } = useCart();
 
-    const item = get(product.id);
-    const count = item ? item.quantity : 0;
+  const item = get(product.id);
+  const count = item ? item.quantity : 0;
 
-    const increment = () => {
-        addItem(product)
-    }
+  const increment = () => {
+    addItem(product);
+  };
 
-    const decrement = () => {
-        removeItem(product.id)
-    }
-    return (<div className="flex flex-row gap-2 items-center">
-    <button onClick={() => decrement()}><MinusCircleIcon size={16} /></button>
-    {count}
-    <button onClick={() => increment()}><PlusCircleIcon size={16} /></button>
-    </div>)
-}
+  const decrement = () => {
+    removeItem(product.id);
+  };
+  return (
+    <div className="flex flex-row gap-2 items-center">
+      <button onClick={() => decrement()}>
+        <MinusCircleIcon size={16} />
+      </button>
+      {count}
+      <button onClick={() => increment()}>
+        <PlusCircleIcon size={16} />
+      </button>
+    </div>
+  );
+};
