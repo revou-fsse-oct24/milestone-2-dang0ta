292f0ea67fbf4807401c899d71ac3012ccd6923a
dang0ta <ikosiriki@gmail.com>
2025-01-10 11:59:34 +0700

    add useResponse custom-hook

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,9 +1,42 @@
-import { useParams } from "react-router-dom";
+import { getProduct } from "@actions/product";
+import { useResponse } from "@hooks/useResponse";
+import { Product } from "@models/product";
+import { Navigate, useParams } from "react-router-dom";
 
-const Product = () => {
-  const { id } = useParams();
+const ProductPage = () => {
+  const { id } = useParams(); // Get the product ID from the URL parameters
+  const { state, response } = useResponse<Product>(async () => {
+    if (!id) {
+      return {
+        status: "error",
+        data: Product.default(),
+        error: "No Product ID provided",
+      };
+    }
 
-  return <div>Product {id}</div>;
+    return getProduct({ id }); // Fetch the product data
+  });
+
+  if (state == "loading" || state == "init") {
+    return <>loading product...</>; // Show loading state
+  }
+
+  if (!response) {
+    return <Navigate to="/" />; // Redirect to home if no response
+  }
+
+  if (response.status == "error") {
+    return <>Error: {response.error}</>; // Show error message
+  }
+
+  const product = response.data; // Extract product data from response
+  return (
+    <div>
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+      <p>Price: {product.price}</p>
+    </div>
+  );
 };
 
-export default Product;
+export default ProductPage;
