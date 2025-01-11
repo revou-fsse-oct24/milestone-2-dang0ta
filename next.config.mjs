d6bafbc5ec2965f07c4de8a1e0f7b435228587c5
dang0ta <ikosiriki@gmail.com>
2025-01-11 15:55:09 +0700

    change build output type to standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-    output: 'export', // Outputs a Single-Page Application (SPA).
     distDir: './dist', // Changes the build output directory to `./dist/`.
+    output: "standalone",
   }
    
   export default nextConfig
