d4a11f30655c39ee671a6e6f72fec451aa18fd27
dang0ta <ikosiriki@gmail.com>
2025-01-11 21:33:44 +0700

    register known image source

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     distDir: './dist', // Changes the build output directory to `./dist/`.
     output: "standalone",
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.imgur.com',
+            },
+
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            }
+        ]
+    }
   }
    
   export default nextConfig
