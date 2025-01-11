0a448b8ce0f9abfe8d36304ac495ae9bab143760
dang0ta <ikosiriki@gmail.com>
2025-01-11 15:29:47 +0700

    add metadata files

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
   name: "DIMS Management App",
-  url: "https://cms.mss.defenxor.com.com",
-  ogImage: "https://www.defenxor.com/images/og/home.jpg",
+  url: "https://shopmart-ashen.vercel.app/",
+  ogImage: "https://shopmart-ashen.vercel.app/images/og/home.jpg",
   description:
     "Control Center Plaform for DIMS Case Management",
   links: {
-    twitter: "https://twitter.com/defenxor",
-    github: "https://github.com/defenxor",
+    twitter: "https://twitter.com/dang0ta",
+    github: "https://github.com/dang0ta",
   },
 }
 
