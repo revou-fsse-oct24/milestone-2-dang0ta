c4c0d9625ebac0bf335fc0a8dce96aee734dc1b2
dang0ta <ikosiriki@gmail.com>
2025-01-12 18:34:24 +0700

    fix incorrect page props

--- src/app/(dashboard)/favorites/page.tsx
+++ src/app/(dashboard)/favorites/page.tsx
@@ -2,18 +2,15 @@
 import { ProductWithCart } from "@/components/product-with-cart";
 import { ScrollBar } from "@/components/ui/scroll-area";
 import { useFavorite } from "@/contexts/favorite-context";
-import { cn } from "@/lib/utils";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 
-const Page = ({ className }: {
-    className?: string;
-}) => {
+const Page = () => {
     const { list } = useFavorite();
     const products = list();
 
     if (products.length === 0) {
         return (
-            <div className={cn(className)}>
+            <div className="flex flex-col gap-4 max-w-[83vw]">
                 <h3 className="text-xl font-bold leading-none capitalize">
                     Favorites
                 </h3>
@@ -28,7 +25,7 @@ const Page = ({ className }: {
     }
 
     return (
-        <div className={cn(className, "flex flex-col gap-4 max-w-[83vw]")}>
+        <div className="flex flex-col gap-4 max-w-[83vw]">
             <h3 className="text-xl font-bold leading-none capitalize">
                 Favorites
             </h3>
