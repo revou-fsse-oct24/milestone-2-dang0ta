e734ffec34a29558f0db3b2e388a1e289ce92b99
dang0ta <ikosiriki@gmail.com>
2025-01-25 00:12:07 +0700

    move app to /shop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,10 @@
-import { getCategoriesURL } from "@/actions/api";
-import { getCategoriesFetcher } from "@/actions/categories";
-import CategoryLoader from "@/components/pages/category-loader";
-import CategorySkeleton from "@/components/category-skeleton";
-import { FilterBanners } from "@/components/filter-banners";
-import useSWR from "swr";
-import PageLayout from "@/components/pages/page-layout";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Page() {
-    const { data, error, isLoading } = useSWR(getCategoriesURL(), getCategoriesFetcher);
-
-    if (isLoading || !data) {
-        return (
-            <PageLayout>
-                <div className="flex flex-col gap-8 py-0">
-                <CategorySkeleton />
-                <CategorySkeleton />
-                <CategorySkeleton />
-            </div>
-            </PageLayout>
-        );
-    }
-
-    if (error) {
-        return <PageLayout>
-            <>Error: {error}</>
-        </PageLayout>;
-    }
-
-    return (
-        <PageLayout>
-            <FilterBanners />
-            {data.map((category) => <CategoryLoader key={category.id} category={category} />)}
-        </PageLayout>
-    );
+    const router = useRouter();
+    
+    useEffect(() => {
+        router.push("/shop")
+    }, [])
 }
-
-
-
