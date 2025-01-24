580858105f9d406427c6cc6dc45cb79793a497f5
dang0ta <ikosiriki@gmail.com>
2025-01-25 00:12:35 +0700

    add custom app with layout option

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import { Providers } from "@/providers/global.provider";
-import "@/styles/globals.css";  
+import "@/styles/globals.css";
+import { NextPage } from "next";
 import { AppProps } from "next/app";
-export default function App({ Component, pageProps }: AppProps) {
-    return <Providers><Component {...pageProps} /></Providers>;
+import { ReactElement, ReactNode } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+};
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+    // Use the layout defined at the page level, if available
+    const getLayout = Component.getLayout ?? ((page) => page);
+    return (
+        <Providers>
+        {getLayout(<Component {...pageProps} />)}
+        </Providers>
+    );
 }
