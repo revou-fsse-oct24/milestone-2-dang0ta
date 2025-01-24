dccc7252fb1d5e2fcdd9aadcda20f697596e2812
dang0ta <ikosiriki@gmail.com>
2025-01-25 00:10:33 +0700

    remove app router

--- src/app/app/(dashboard)/layout.tsx
+++
@@ -1,8 +0,0 @@
-import PageLayout from "@/components/page-layout";
-import { ReactNode } from "react";
-
-export default function Layout({children}: {readonly children: ReactNode})  {
-    return(
-        <PageLayout>{children}</PageLayout>
-    )
-}
