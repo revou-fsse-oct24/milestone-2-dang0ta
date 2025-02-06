6dee2785462ec2e2ea8e5d813a5ba3eac78384fa
Riki Syahputra <ikosiriki@gmail.com>
2025-02-06 17:50:03 +0700

    Add About us page (#4)
    * add link to about-us page
    
    * add about-us page

--- src/components/pages/page-layout.tsx
+++ src/components/pages/page-layout.tsx
@@ -8,16 +8,22 @@ import { ReactNode } from "react";
 import MainContainer from "@/components/main-container";
 import { MainBreadcrumb } from "@/components/main-breadcrumb";
 import { UserSidebar } from "@/components/pages/navigation/user-sidebar";
+import { StoreIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const PageLayout = ({ children }: { children: ReactNode; }) => {
     return (
         <>
             <MainSidebar footer={<UserSidebar />} />
             <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-                    <SidebarTrigger className="-ml-1" />
-                    <Separator orientation="vertical" className="mr-2 h-4" />
-                    <MainBreadcrumb />
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
+                    <div className="flex flex-row items-center gap-2">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator orientation="vertical" className="mr-2 h-4" />
+                        <MainBreadcrumb />
+                    </div>
+                    <Link href="about-us"><Button size="sm" variant="ghost">about us <StoreIcon /></Button></Link>
                 </header>
                 <MainContainer>{children}</MainContainer>
             </SidebarInset>
---
+++ src/pages/about-us.tsx
@@ -0,0 +1,23 @@
+import PageLayout from "@/components/pages/page-layout";
+
+export default function AboutUsPage() {
+    return (
+
+        <PageLayout>
+            <div className="p-4 max-w-screen-xl flex flex-col space-y-2">
+                <h1 className="text-3xl font-bold tracking-tight">About Us</h1>
+                <p>Welcome to Shopmart, your go-to online e-commerce platform for all your shopping needs. Built using Next.js, Shadcn, and the Platzi Fake Store API, we aim to provide a seamless and enjoyable shopping experience.</p>
+                <h2 className="font-semibold leading-8 tracking-tight">Our Mission</h2>
+                <p>At Shopmart, our mission is to offer a wide variety of products at competitive prices, ensuring customer satisfaction and convenience.</p>
+                <h2 className="font-semibold leading-8 tracking-tight">Technologies We Use</h2>
+                <ul>
+                    <li><strong>Next.js:</strong> A powerful React framework for building fast and scalable web applications.</li>
+                    <li><strong>Shadcn:</strong> A design system that helps us create beautiful and consistent user interfaces.</li>
+                    <li><strong>Platzi Fake Store API:</strong> A mock API that provides us with realistic product data for development and testing purposes.</li>
+                </ul>
+                <h2 className="font-semibold leading-8 tracking-tight">Contact Us</h2>
+                <p>If you have any questions or feedback, feel free to reach out to us at support@shopmart.com.</p>
+            </div>
+        </PageLayout>
+    );
+}
